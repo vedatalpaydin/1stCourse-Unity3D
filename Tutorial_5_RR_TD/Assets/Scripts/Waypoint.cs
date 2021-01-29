fcb44c003ec204b48de1052d4c44348eccd1cd14
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-01-29 12:04:51 +0300

    Tutorial_5_RR_TD

---
+++ Tutorial_5_RR_TD/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public bool isExplored = false;
+    public Waypoint exploredFrom;
+    public bool isPlaceable = true;
+
+    Vector2Int gridPos;
+
+    const int gridSize = 2;
+    const string towerParentName = "Towers";
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / gridSize),
+            Mathf.RoundToInt(transform.position.z / gridSize)
+        );
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0)) // left click
+        {
+            if (isPlaceable)
+            {
+                FindObjectOfType<TowerFactory>().AddTower(this);
+            }
+            else
+            {
+                print("Can't place here");
+            }
+        }                
+    }
+}
